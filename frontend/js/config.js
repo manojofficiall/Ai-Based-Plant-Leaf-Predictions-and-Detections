45765f70043ae2b040dfdd4d2cc1d0fecd6dadc4
Manojkumar C <manojkumar332629@gmail.com>
2026-09-11 11:32:35 +0530

    Add API base URL to configuration

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,3 +2,5 @@
 const CONFIG = {
     GOOGLE_CLIENT_ID: '101090810891-n8bb6gib7gs2gdjmpfos9a7npf0fnafi.apps.googleusercontent.com'
 };
+const API_BASE_URL =
+    "https://ai-based-plant-leaf-predictions-and-sz4u.onrender.com";
