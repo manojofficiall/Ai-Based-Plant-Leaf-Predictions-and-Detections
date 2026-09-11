9a84aef1a3e8d00598268d8235fd0909c7ad38d5
Manojkumar C <manojkumar332629@gmail.com>
2026-09-11 14:37:45 +0530

    Update CORS_ALLOW_CREDENTIALS for specific origin

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = "https://plantleafpredictions.netlify.app"
 
 ROOT_URLCONF = 'django_project.urls'
 
